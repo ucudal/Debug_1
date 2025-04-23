898de81124f8510fb3cf94f6723624dca0f7394e
Ulises <ulises.rattin@gmail.com>
2025-04-23 20:41:53 -0300

    Arreglos en age y en familyname

--- src/Library/Person.cs
+++ src/Library/Person.cs
@@ -42,7 +42,7 @@ namespace Ucu.Poo.Persons
         {
             get
             {
-                return $"{this.Name}{this.FamilyName}";
+                return $"{this.Name} {this.FamilyName}";
             }
         }
 
@@ -59,7 +59,7 @@ namespace Ucu.Poo.Persons
             get
             {
                 DateTime today = DateTime.Today;
-                int age = this.BirthDate.Year - today.Year;
+                int age =today.Year - this.BirthDate.Year;
 
                 if (today.Month < this.BirthDate.Month || (today.Month == this.BirthDate.Month && today.Day < this.BirthDate.Day))
                 {
